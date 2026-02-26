8635bc33f90cbd505a9adc1c0047e6da1d29eb58
Trushant Adeshara <trushantadeshara99@gmail.com>
2026-02-26 12:59:14 -0800

    Add multi-stage docker workflow for GLB to USDA conversion

--- aic_utils/aic_isaac/aic_converter.sh
+++ aic_utils/aic_isaac/aic_converter.sh
@@ -11,7 +11,10 @@ cd $DOCKER_DIR_PATH
 # -- Build ws_aic
 
 # ARG to build aic_converter docker image 
-# docker compose build converter
+docker compose build converter
+
+# Debug aic_isaaclab_stage artifacts
+# docker cp $(docker create --rm ghcr.io/intrinsic-dev/aic/aic_converter):/workspace/isaaclab/artifacts $SCRIPT_DIR/artifacts
 
 # ARG to copy aic.sdf to build_artifacts on host
 # docker cp $(docker create --rm ghcr.io/intrinsic-dev/aic/aic_converter):/tmp/aic.sdf $SCRIPT_DIR/build_artifacts/aic.sdf
